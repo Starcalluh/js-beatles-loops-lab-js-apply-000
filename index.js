486b73aa2b93b5043ed42e9a19c92f8914927917
George <starcalluh@live.com>
2019-08-10 09:40:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ function iLoveTheBeatles(number) {
  } while (number < 15);
  return array
 }
+var facts = ["He was the last Beatle to learn to drive" , "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 function johnLennonFacts(facts) {
   let array = []
   let i = 0
   while(i < facts.length) 
   {
     array.push(facts + "!!!");
-    i++
+    i++ ;
   }
   return array
 }
